21eca6e3d330ec0fedc513930030183d4ff8a739
Matthieu Moinvaziri <m.moinvaziri@gmail.com>
2021-05-09 01:30:00 +0200

    Updated pinout

--- Board/PinoutConfig.hpp
+++ Board/PinoutConfig.hpp
@@ -2,15 +2,15 @@
 #pragma once
 
 #ifndef HARDWARE_PIN_COUNT
-# define HARDWARE_PIN_COUNT 0
+# define HARDWARE_PIN_COUNT 3
 #endif
 
 namespace Pin
 {
     static constexpr int Array[] = {
-        40, 38, 36
+        29, 28, 27
     };
     static constexpr int Count = HARDWARE_PIN_COUNT;
 
-    static_assert(sizeof(Array) / sizeof(*Array) > Count, "Pin count too high for given pin array");
+    static_assert(sizeof(Array) / sizeof(*Array) >= Count, "Pin count too high for given pin array");
 }
